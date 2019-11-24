20b08d55b36b1c0abaac1c74a9f15f76530f5bad
RobinAlex <872576923@qq.com>
2019-11-24 23:19:39 +0000

    Init commit

---
+++ src/Registry.py
@@ -0,0 +1,36 @@
+#Extract pinned file from registry
+
+from winreg import *
+
+def fetchRegistry():
+        MRUFiles = {}
+        wordKey = None
+
+        reg = ConnectRegistry(None, HKEY_CURRENT_USER)
+        wordKey = OpenKeyEx(reg,r"Software\Microsoft\Office\16.0\Word\User MRU\ADAL_F594B3B642FB17C41BF1285F133AD15D55BA76E8CB5C5CECD079FFED29C99780\File MRU")
+        #excelKey = OpenKeyEx(reg,r"Software\Microsoft\Office\16.0\Excel\User MRU\ADAL_F594B3B642FB17C41BF1285F133AD15D55BA76E8CB5C5CECD079FFED29C99780\File MRU")
+        #pptKey = OpenKeyEx(reg,r"Software\Microsoft\Office\16.0\PowerPoint\User MRU\ADAL_F594B3B642FB17C41BF1285F133AD15D55BA76E8CB5C5CECD079FFED29C99780\File MRU")
+
+        i = 0
+        while True:
+            try:
+                value = EnumValue(wordKey,i)
+                if True:#TODO
+                    fileInfo = extractInfo(value)
+                    MRUFiles[fileInfo[0]] = fileInfo[1]
+                i = i+1
+            except OSError:  # EOF
+                break
+        return MRUFiles
+
+def extractInfo(value):
+    posOfAsterisk = value[1].find("*")
+    filepath  = value[1][posOfAsterisk+1:]
+    posOfSlash = filepath.rfind('\\')
+    fileName = filepath[posOfSlash+1:]
+    return fileName, filepath
+
+def isPinned(value):
+    if "F00000001" in value[1]:
+        return True
+    return False
---
+++ src/main.py
@@ -0,0 +1,43 @@
+from fuzzywuzzy import process
+import os
+from random import random
+
+from wox import Wox, WoxAPI
+from src import Registry
+
+
+# TODO Lowercase
+
+class Main():
+    MRUFiles = {}
+
+    def buildReg(self):
+        self.MRUFiles = Registry.fetchRegistry()
+        return
+
+    def query(self, query):
+        if not self.MRUFiles:
+            self.buildReg()
+        returnResults = []
+        results = process.extract(query, self.MRUFiles.keys(), limit=10)
+        for result in results:
+             formattedPath = self.MRUFiles[result[0]]
+             returnResults.append({
+                 "Title": result[0],
+                 "SubTitle": formattedPath,
+                 "IcoPath": "Res/word.ico",
+                  "JsonRPCAction": {
+                     "method": "openFile",
+                      "parameters": [formattedPath],
+                      "dontHideAfterAction": False
+                  }
+             })
+        return returnResults
+
+    def openFile(self, filePath):
+        os.startfile(filePath)
+        # TODO should be more secure
+
+# Necessary code
+if __name__ == "__main__":
+    Main()
